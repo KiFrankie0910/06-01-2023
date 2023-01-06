b643cc532225d606b346088329382f3822dc20ee
Ki Frankie <106165688+kifrankie0910@users.noreply.github.com>
2023-01-06 15:20:32 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+})
